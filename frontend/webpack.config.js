c7d58e6a6ebc9d64809916a65625e7c29ee41ed2
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2018-04-05 15:12:39 +1000

    remove config. dont use it anywhere anymore

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -22,14 +22,6 @@ var config = {
             }
         ]
     },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-          API_ROOT: JSON.stringify(process.env.API_ROOT),
-        },
-      }),
-    ]
 };
 
 module.exports = config;
